68ae93e2d935bee9989a1ed534877bd68d2ac842
Brian Hoang <brian.hd.hoang@codurance.com>
2022-02-14 16:34:40 +0000

    refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,17 +9,17 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!isAgedBrie(item)
+                && !isBackstagePass(item)) {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!isSulfuras(item)) {
                         item.quality = downgradeQuality(item);
                     }
                 }
             } else {
                     item.quality = increaseQuality(item);
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isBackstagePass(item)) {
                         if (item.sellIn < 11) {
                                 item.quality = increaseQuality(item);
                         }
@@ -29,17 +29,25 @@ class GildedRose {
             decreaseSellIn(item);
 
             if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
+                if (isAgedBrie(item)) {
                     item.quality = increaseQuality(item);
                 }
 
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        item.quality = downgradeQuality(item);
-                    } else {
-                        item.quality = 0;
-                }
+                item.quality = downgradeQuality(item);
             }
         }
+    }
+
+    private boolean isBackstagePass(Item item) {
+       return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isAgedBrie(Item item) {
+       return item.name.equals("Aged Brie");
+    }
+
+    private boolean isSulfuras(Item item) {
+       return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
 
     private void decreaseSellIn(Item item) {
@@ -49,15 +57,20 @@ class GildedRose {
     }
 
     private int increaseQuality(Item item) {
-       int qualityMaximum = item.name.equals("Sulfuras, Hand of Ragnaros") ? 80 : 50;
+       int qualityMaximum = isSulfuras(item) ? 80 : 50;
 
        return Integer.min(qualityMaximum, item.quality + 1);
     }
 
     private int downgradeQuality(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros") || item.name.equals("Aged Brie")) {
+        if (isSulfuras(item) || isAgedBrie(item)) {
             return item.quality;
         }
+
+        if (isBackstagePass(item) && item.sellIn == 0) {
+            return 0;
+        }
+
         return Integer.max(0, item.quality - 1);
     }
 }
