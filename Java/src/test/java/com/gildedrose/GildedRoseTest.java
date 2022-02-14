a1570605cdd93a21ed0ff30747b180c653be98ad
Brian Hoang <brian.hd.hoang@codurance.com>
2022-02-14 15:57:25 +0000

    golden master test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -23,4 +23,64 @@ class GildedRoseTest {
         assertEquals(1, app.items[0].sellIn);
     }
 
+    @Test
+    void not_allow_quality_to_go_below_zero() {
+        Item[] items = new Item[] { new Item("chocolate bar", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void should_degrade_twice_as_fast_when_sell_in_is_below_zero() {
+        Item[] items = new Item[] { new Item("flowers", 1, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
+    }
+
+    @Test
+    void golden_master_test() {
+        Item[] items = new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)
+        };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        Item regularItem = app.items[0];
+        assertEquals(9, regularItem.sellIn);
+        assertEquals(19, regularItem.quality);
+
+        Item agedBrie = app.items[1];
+        assertEquals(1, agedBrie.sellIn);
+        assertEquals(1, agedBrie.quality);
+
+        Item sulfuras = app.items[3];
+        assertEquals(0, sulfuras.sellIn);
+        assertEquals(80, sulfuras.quality);
+
+        Item expiredSulfuras = app.items[4];
+        assertEquals(-1, expiredSulfuras.sellIn);
+        assertEquals(80, expiredSulfuras.quality);
+
+        Item backstagePass = app.items[5];
+        Item lessThanTenDaysBackstagePass = app.items[6];
+        Item lessThanFiveDaysBackstagePass = app.items[7];
+
+        assertEquals(14, backstagePass.sellIn);
+        assertEquals(21, backstagePass.quality);
+        assertEquals(9, lessThanTenDaysBackstagePass.sellIn);
+        assertEquals(50, lessThanTenDaysBackstagePass.quality);
+        assertEquals(4, lessThanFiveDaysBackstagePass.sellIn);
+        assertEquals(50, lessThanFiveDaysBackstagePass.quality);
+    }
 }
