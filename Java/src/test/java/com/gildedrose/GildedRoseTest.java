3cc2f2081ee748d918e451533050bf1cf2d6eee4
Brian Hoang <brian.hd.hoang@codurance.com>
2022-02-14 15:37:54 +0000

    green

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,13 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void should_lower_quality_and_sell_in_of_item_after_one_day() {
+        Item[] items = new Item[] { new Item("chocolate bar", 2, 2) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].quality);
+        assertEquals(1, app.items[0].sellIn);
+    }
+
 }
