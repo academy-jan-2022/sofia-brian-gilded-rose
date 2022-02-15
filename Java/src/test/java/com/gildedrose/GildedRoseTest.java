6c552b7093f78aadd6480fee4752169a710df6bf
Brian Hoang <brian.hd.hoang@codurance.com>
2022-02-15 10:05:43 +0000

    snapshot test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,6 +2,8 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
@@ -41,7 +43,7 @@ class GildedRoseTest {
     }
 
     @Test
-    void golden_master_test() {
+    void acceptence_test() {
         Item[] items = new Item[] {
             new Item("+5 Dexterity Vest", 10, 20), //
             new Item("Aged Brie", 2, 0), //
@@ -87,5 +89,74 @@ class GildedRoseTest {
         Item conjured = app.items[8];
         assertEquals(2, conjured.sellIn);
         assertEquals(4, conjured.quality);
+    }
+
+    @Test
+    void snapshot_test() {
+        Item[] items = new Item[] {
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49),
+            new Item("Conjured Mana Cake", 3, 6) };
+
+        GildedRose app = new GildedRose(items);
+
+        StringBuilder result = new StringBuilder();
+
+        IntStream.range(0, 3).forEach(i -> {
+            result.append("-------- day ").append(i).append(" --------");
+            result.append("\n");
+            result.append("name, sellIn, quality");
+            result.append("\n");
+            for (Item item : items) {
+                result.append(item);
+                result.append("\n");
+            }
+            result.append("\n");
+            app.updateQuality();
+        });
+
+        String expected = "-------- day 0 --------\n" +
+            "name, sellIn, quality\n" +
+            "+5 Dexterity Vest, 10, 20\n" +
+            "Aged Brie, 2, 0\n" +
+            "Elixir of the Mongoose, 5, 7\n" +
+            "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+            "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 15, 20\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 10, 49\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 1, 49\n" +
+            "Conjured Mana Cake, 3, 6\n" +
+            "\n" +
+            "-------- day 1 --------\n" +
+            "name, sellIn, quality\n" +
+            "+5 Dexterity Vest, 9, 19\n" +
+            "Aged Brie, 1, 1\n" +
+            "Elixir of the Mongoose, 4, 6\n" +
+            "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+            "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 14, 21\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 9, 50\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 0, 50\n" +
+            "Conjured Mana Cake, 2, 4\n" +
+            "\n" +
+            "-------- day 2 --------\n" +
+            "name, sellIn, quality\n" +
+            "+5 Dexterity Vest, 8, 18\n" +
+            "Aged Brie, 0, 2\n" +
+            "Elixir of the Mongoose, 3, 5\n" +
+            "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+            "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 13, 22\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 8, 50\n" +
+            "Backstage passes to a TAFKAL80ETC concert, -1, 0\n" +
+            "Conjured Mana Cake, 1, 2";
+
+        assertEquals(expected, result.toString().trim());
     }
 }
