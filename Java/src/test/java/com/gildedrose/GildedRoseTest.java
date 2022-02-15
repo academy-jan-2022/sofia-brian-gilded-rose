1c0b0b0cdde45e6960a80562330d7701e86c2185
sofia <sofia.carballo@codurance.com>
2022-02-15 10:17:27 +0100

    Change testcases for backstage pass

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -50,7 +50,7 @@ class GildedRoseTest {
             new Item("Sulfuras, Hand of Ragnaros", -1, 80),
             new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
             new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49)
         };
         GildedRose app = new GildedRose(items);
 
@@ -80,7 +80,7 @@ class GildedRoseTest {
         assertEquals(21, backstagePass.quality);
         assertEquals(9, lessThanTenDaysBackstagePass.sellIn);
         assertEquals(50, lessThanTenDaysBackstagePass.quality);
-        assertEquals(4, lessThanFiveDaysBackstagePass.sellIn);
-        assertEquals(50, lessThanFiveDaysBackstagePass.quality);
+        assertEquals(-1, lessThanFiveDaysBackstagePass.sellIn);
+        assertEquals(0, lessThanFiveDaysBackstagePass.quality);
     }
 }
